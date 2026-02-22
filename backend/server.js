3ffb4c99146f3570a0d0639b892444e4d19fc09c
Mahender <tnmahender@gmail.com>
2026-02-22 12:56:56 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser=require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const connectToDatabase = require("./database/db");
 const authRoutes = require("./routes/auth-routes");
 const noteRoutes = require("./routes/note-routes");
-const cors=require('cors')
+const cors = require("cors");
 
 const app = express();
 
 //enable cors
-app.use(cors({
-  origin: "https://note-app-frontend-dusky.vercel.app/",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://note-app-frontend-dusky.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 //database connection
 connectToDatabase();
@@ -26,14 +28,10 @@ app.use(cookieParser());
 app.use("/", authRoutes);
 app.use("/", noteRoutes);
 
-
-
 const PORT = process.env.PORT;
-// app.listen(PORT, () => {
-//   console.log(`Server is running at port ${PORT}`);
-// });
+
 app.get("/", (req, res) => {
   res.status(200).send("Backend is running ✅");
 });
 
-module.exports=app
+module.exports = app;
